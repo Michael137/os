e2f05ea9e4fd9908a441e26a34f0ed1ba841abc1
Michael Buch <michaelbuch12@gmail.com>
2019-02-10 22:36:39 +0000

    more graphs

--- l41-labs/lab1/benchmark.sh
+++ l41-labs/lab1/benchmark.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
+# I miss bash...
 EXE=$1
 FLAGS=$2
 FILE=$3
 D_SCRIPT=$4
 OUT_FILE=$5
-RUNS=1
+RUNS=2
 FILE_SIZE=16777216
+BUF_SIZE=16384
 
 echo "Creating benchmark file of size ${FILE_SIZE}"
 ${EXE} -c -t ${FILE_SIZE} ${FILE}
 echo "Starting benchmark with: ${FLAGS} on ${FILE}"
 
-# I miss bash...
+# Use for constant IO
+# for i in 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216
 for i in 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216
 do
+	# Use for constant IO
+#	${EXE} -c -t ${i} ${FILE}
+#	cmd="${EXE} ${FLAGS} -b ${BUF_SIZE} -t ${i} ${FILE}"
 	cmd="${EXE} ${FLAGS} -b ${i} -t ${FILE_SIZE} ${FILE}"
 	echo "Executing ${cmd}"
 	echo "Starting DTrace: ${D_SCRIPT}"
@@ -28,7 +34,7 @@ do
 	io_pid=$!
 
 	# Start DTrace here
-	dtrace -p $! -s ${D_SCRIPT} > ${OUT_FILE}_${i} 2>&1 &
+	dtrace -p $! -s ${D_SCRIPT} -o ${OUT_FILE}_${i} &
 	dtrace_pid=$!
 
 	# Wait for I/O to finish
