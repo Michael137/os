d5f0161b3b0093550f19f6133cf957b000e54d86
Michael Buch <michaelbuch12@gmail.com>
2019-02-10 17:55:10 +0000

    move files to separate folder

--- l41-labs/lab1/static_io16mb_bufferVar_ioPerf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for ctr in 1 2 4 8 16 32 64 128 256 512 1024 4096 8192 16384 32768 65536
-do
-	buf=$((512 * (2 ^ $ctr)))
-	echo $buf
-	../io/io-static -r -B -d -q -t $buf iofile
-done
