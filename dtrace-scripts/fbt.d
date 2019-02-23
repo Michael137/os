d17eb64fd3c95eb855efb127b6387785fda8b7cc
Michael Buch <michaelbuch12@gmail.com>
2019-02-23 17:43:18 +0000

    trace virtual mem function boundaries

---
+++ dtrace-scripts/fbt.d
@@ -0,0 +1,38 @@
+syscall::clock_gettime:return
+/execname == $$1 && !self->in_bench/
+{
+	self->in_bench = 1;
+	self->bts = vtimestamp;
+}
+
+syscall::clock_gettime:entry
+/execname == $$1 && self->in_bench/
+{
+	@bench_time["vtime"] = sum(vtimestamp - self->bts);
+	self->in_bench = 0;
+}
+
+/*fbt::*page*:entry*/
+/*fbt:::entry*/
+/*fbt::vm_*:entry*/
+fbt::vm_fault:entry,
+fbt::vm_page*:entry,
+fbt::pmap*:entry
+/execname == $$1 && self->in_bench/
+{
+	self->pts = timestamp;
+}
+
+fbt::vm_fault:return,
+fbt::vm_page*:return,
+fbt::pmap*:return
+/execname == $$1 && self->in_bench/
+{
+	@fbt[probefunc] = sum(timestamp - self->pts);
+}
+
+END
+{
+	printa(@fbt);
+	printa(@bench_time);
+}
--- dtrace-scripts/syscall.d
+++ dtrace-scripts/syscall.d
@@ -45,22 +45,9 @@ fbt::trap:return
 	@trap_total = sum(length);
 }
 
-fbt::*page*:entry
-/execname == $$1 && self->in_bench/
-{
-	self->pts = timestamp;
-}
-
-fbt::*page*:return
-/execname == $$1 && self->in_bench/
-{
-	@fbt[probefunc] = sum(timestamp - self->pts);
-}
-
 END
 {
 	printa(@syscall_times);
-	printa(@fbt);
 
 	printf("syscalls:\n"); printa(@syscalls); printa(@syscall_total);
 	printf("traps:\n"); printa(@traps); printa(@trap_total);
