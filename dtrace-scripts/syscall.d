e84b1025027b7aa1a96922499772c03f49ad0217
Michael Buch <michaelbuch12@gmail.com>
2019-02-23 12:24:06 +0000

    add clock_gettime trap for syscall timing

--- dtrace-scripts/syscall.d
+++ dtrace-scripts/syscall.d
@@ -1,9 +1,20 @@
 /*
  * Invoke as: dtrace -s syscall.d "<exec name>"
  */
+syscall::clock_gettime:return
+/execname == $$1 && !self->in_bench/
+{
+	self->in_bench = 1
+}
+
+syscall::clock_gettime:entry
+/execname == $$1 && self->in_bench/
+{
+	self->in_bench = 0
+}
 
 syscall:::entry
-/execname == $$1/
+/execname == $$1 && self->in_bench && probefunc != "clock_gettime"/
 {
 	@syscalls = count();
 	self->start = timestamp;
