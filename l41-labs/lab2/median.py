b6b38db407c556b50f298f2f2188de7187dd3e5f
Michael Buch <michaelbuch12@gmail.com>
2019-03-01 15:36:36 +0000

    do median_test analysis (and t-test)

---
+++ l41-labs/lab2/median.py
@@ -0,0 +1,49 @@
+from scipy import stats
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('GTKAgg')
+import matplotlib.pyplot as plt
+
+'''
+    TODO:
+        for each thread mode
+            local vs pipe
+            local vs local -s
+            pmc for local and local -s
+            pmc for pipe and local
+'''
+lcl_s = pd.read_pickle("pkls/pipe_2thread.pkl")
+lcl = pd.read_pickle("pkls/pipe_2proc.pkl")
+
+toCompare = {}
+pvalues = []
+pvalues_ttest = []
+for frame in lcl_s:
+    size = frame['buffersize']
+    if size not in toCompare:
+        toCompare[size] = [frame['speeds']]
+    else:
+        toCompare[size] += frame['speeds']
+
+for frame in lcl:
+    size = frame['buffersize']
+    if size not in toCompare:
+        toCompare[size] = [frame['speeds']]
+    else:
+        toCompare[size].append(frame['speeds'])
+
+for size,res in toCompare.iteritems():
+    stat, p, med, tbl = stats.median_test(res[0], res[1])
+    print(str(size) + ":\n\t" + str(p))
+    ttest = stats.ttest_ind(res[0], res[1])
+    pvalues.append(p)
+    pvalues_ttest.append(ttest.pvalue)
+
+fig=plt.figure()
+ax1=fig.add_subplot(2,1,1)
+ax2=fig.add_subplot(2,1,2)
+
+plt.plot(pvalues)
+plt.plot(pvalues_ttest)
+fig.savefig("pipe_2thread_pipe_2proc.png")
