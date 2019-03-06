ecbe4f5b871894db55ed71f6b7bf017129ab1499
Michael Buch <michaelbuch12@gmail.com>
2019-03-06 00:29:07 +0000

    dtrace page zeroing

---
+++ l41-labs/lab2/page.d
@@ -0,0 +1,21 @@
+syscall::clock_gettime:return
+/execname == "ipc-static" && !in_bench/
+{
+	in_bench = 1
+}
+
+syscall::clock_gettime:entry
+/execname == "ipc-static" && in_bench/
+{
+	in_bench = 0;
+	stop_tracing = 1;
+}
+
+fbt::pmap_copy_page:entry,
+fbt::pmap_zero_page:entry
+/execname == "ipc-static"
+	&& in_bench
+	&& !stop_tracing/
+{
+	@["count", probefunc] = count()
+}
