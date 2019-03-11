bb5186d05a14b0a893d2afe2a3e688ffdda70a29
Michael Buch <mb2244@cl.cam.ac.uk>
2019-03-11 12:52:40 +0000

    init tcp investigations

--- lab3/run.sh
+++ lab3/run.sh
@@ -1,30 +1,26 @@
 #!/bin/sh
 
 # uiomove
-./benchmark.sh ipc/ipc-static "" pipe 1thread uio.d uio.out 10
-./benchmark.sh ipc/ipc-static "" local 1thread uio.d uio.out 10
-./benchmark.sh ipc/ipc-static "" pipe 2thread uio.d uio.out 10
-./benchmark.sh ipc/ipc-static "" pipe 2proc uio.d uio.out 10
-./benchmark.sh ipc/ipc-static "" local 2thread uio.d uio.out 10
-./benchmark.sh ipc/ipc-static "" local 2proc uio.d uio.out 10
-./benchmark.sh ipc/ipc-static "-s" local 2thread uio.d uio.out 10
-./benchmark.sh ipc/ipc-static "-s" local 2proc uio.d uio.out 10
+# ./benchmark.sh ipc/ipc-static "" pipe 2thread uio.d uio.out 10
+# ./benchmark.sh ipc/ipc-static "" pipe 2proc uio.d uio.out 10
+# ./benchmark.sh ipc/ipc-static "" local 2thread uio.d uio.out 10
+# ./benchmark.sh ipc/ipc-static "" local 2proc uio.d uio.out 10
+# ./benchmark.sh ipc/ipc-static "-s" local 2thread uio.d uio.out 10
+# ./benchmark.sh ipc/ipc-static "-s" local 2proc uio.d uio.out 10
 
 # Off-cpu
 # ./benchmark.sh ipc/ipc-static "" local 2proc offcputime.d offcpu.out 10
 # ./benchmark.sh ipc/ipc-static "" local 2thread offcputime.d offcpu.out 10
 # ./benchmark.sh ipc/ipc-static "-s" local 2proc offcputime.d offcpu.out 10
 # ./benchmark.sh ipc/ipc-static "-s" local 2thread offcputime.d offcpu.out 10
-# ./benchmark.sh ipc/ipc-static "" pipe 2proc offcputime.d offcpu.out 10
-# ./benchmark.sh ipc/ipc-static "" pipe 2thread offcputime.d offcpu.out 10
  
 # prefault/vm_fault/pmap_fault
-./benchmark.sh ipc/ipc-static "" pipe 2proc pmap.d pmap.out 10
-./benchmark.sh ipc/ipc-static "" pipe 2thread pmap.d pmap.out 10
-./benchmark.sh ipc/ipc-static "-s" local 2proc pmap.d pmap.out 10
-./benchmark.sh ipc/ipc-static "-s" local 2thread pmap.d pmap.out 10
-./benchmark.sh ipc/ipc-static "" local 2thread pmap.d pmap.out 10
-./benchmark.sh ipc/ipc-static "" local 2proc pmap.d pmap.out 10
+# ./benchmark.sh ipc/ipc-static "" pipe 2proc pmap.d pmap.out 10
+# ./benchmark.sh ipc/ipc-static "" pipe 2thread pmap.d pmap.out 10
+# ./benchmark.sh ipc/ipc-static "-s" local 2proc pmap.d pmap.out 10
+# ./benchmark.sh ipc/ipc-static "-s" local 2thread pmap.d pmap.out 10
+# ./benchmark.sh ipc/ipc-static "" local 2thread pmap.d pmap.out 10
+# ./benchmark.sh ipc/ipc-static "" local 2proc pmap.d pmap.out 10
 
 # rw
 # ./benchmark.sh ipc/ipc-static "" pipe 1thread rw_count.d rw.out 3
@@ -57,3 +53,8 @@
 # ./benchmark.sh ipc/ipc-static "" local 2thread page.d page.out 10
 # ./benchmark.sh ipc/ipc-static "-s" local 2proc page.d page.out 10
 # ./benchmark.sh ipc/ipc-static "-s" local 2thread page.d page.out 10
+
+# rw timing
+./benchmark.sh ipc/ipc-static "" pipe 2proc rw_time.d rw_time.out 10
+./benchmark.sh ipc/ipc-static "" local 2proc rw_time.d rw_time.out 10
+./benchmark.sh ipc/ipc-static "-s" local 2proc rw_time.d rw_time.out 10
