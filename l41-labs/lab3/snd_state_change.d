d919ecf6b0a1f5fb40edf9d6e36f7e57a56d3ba7
Michael Buch <michaelbuch12@gmail.com>
2019-03-25 02:28:04 +0000

    translate tcp flags during tracing

--- l41-labs/lab3/snd_state_change.d
+++ l41-labs/lab3/snd_state_change.d
@@ -3,16 +3,19 @@
 fbt::tcp_state_change:entry
 /args[0]->t_inpcb->inp_inc.inc_ie.ie_fport == htons(10141)/
 {
-	trace(ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_lport));
-	trace(ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_fport));
+/*	trace(ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_lport));
+	trace(ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_fport));*/
 	trace(tcp_state_string[args[0]->t_state]);
 	trace(tcp_state_string[args[1]]);
-	printf("[%s", (args[0])->t_flags & TH_FIN ? "FIN|" : "");
-	printf("%s", (args[0])->t_flags & TH_SYN ? "SYN|" : "");
-	printf("%s", (args[0])->t_flags & TH_RST ? "RST|" : "");
-	printf("%s", (args[0])->t_flags & TH_PUSH ? "PUSH|" : "");
-	printf("%s", (args[0])->t_flags & TH_ACK ? "ACK|" : "");
-	printf("%s", (args[0])->t_flags & TH_URG ? "URG|" : "");
-	printf("%s", (args[0])->t_flags & TH_ECE ? "ECE|" : "");
-	printf("%s]", (args[0])->t_flags & TH_CWR ? "CWR" : "");
+	/*printf("%s", (args[0])->t_flags & TF_RST ? "RST|" : "");*/
+	printf("[%s", (args[0])->t_flags & 0x000001 ? "ACKNOW|" : "");
+	printf("%s", (args[0])->t_flags & 0x000010 ? "SENTFIN|" : "");
+	printf("%s", (args[0])->t_flags & 0x000400 ? "NEEDSYN|" : "");
+	printf("%s", (args[0])->t_flags & 0x000800 ? "NEEDFIN|" : "");
+	printf("%s", (args[0])->t_flags & 0x80000000 ? "FASTOPEN|" : "");
+	printf("%s", (args[0])->t_flags & 0x1000000 ? "TSO|" : "");
+	printf("%s", (args[0])->t_flags & 0x2000000 ? "TOE|" : "");
+	printf("%s", (args[0])->t_flags & 0x8000000 ? "CWR|" : "");
+	printf("%s", (args[0])->t_flags & 0x10000000 ? "ECE|" : "");
+	printf("%s]", (args[0])->t_flags & 0x20000000 ? "CONGRECOVERY" : "");
 }
