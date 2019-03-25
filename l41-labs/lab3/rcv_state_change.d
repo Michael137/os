66df5256cb0c0deacc3e95c3aa791fbe9e5d3139
Michael Buch <michaelbuch12@gmail.com>
2019-03-25 09:51:33 +0000

    remove unnecessary files; tweak sender transition dtrace

--- l41-labs/lab3/rcv_state_change.d
+++ l41-labs/lab3/rcv_state_change.d
@@ -3,8 +3,19 @@
 fbt::tcp_state_change:entry
 /args[0]->t_inpcb->inp_inc.inc_ie.ie_lport == htons(10141)/
 {
-	trace(ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_lport));
-	trace(ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_fport));
+	/*trace(ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_lport));
+	trace(ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_fport));*/
 	trace(tcp_state_string[args[0]->t_state]);
 	trace(tcp_state_string[args[1]]);
+	/*printf("%s", (args[0])->t_flags & TF_RST ? "RST|" : "");*/
+	printf("[%s", (args[0])->t_flags & 0x000001 ? "ACKNOW|" : "");
+	printf("%s", (args[0])->t_flags & 0x000010 ? "SENTFIN|" : "");
+	printf("%s", (args[0])->t_flags & 0x000400 ? "NEEDSYN|" : "");
+	printf("%s", (args[0])->t_flags & 0x000800 ? "NEEDFIN|" : "");
+	printf("%s", (args[0])->t_flags & 0x80000000 ? "FASTOPEN|" : "");
+	printf("%s", (args[0])->t_flags & 0x1000000 ? "TSO|" : "");
+	printf("%s", (args[0])->t_flags & 0x2000000 ? "TOE|" : "");
+	printf("%s", (args[0])->t_flags & 0x8000000 ? "CWR|" : "");
+	printf("%s", (args[0])->t_flags & 0x10000000 ? "ECE|" : "");
+	printf("%s]", (args[0])->t_flags & 0x20000000 ? "CONGRECOVERY" : "");
 }
