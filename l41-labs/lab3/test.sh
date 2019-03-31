1eced500f13105fb050a58c50c9e712aee1991db
Michael Buch <michaelbuch12@gmail.com>
2019-03-31 23:31:53 +0000

    time-sequence plot

--- l41-labs/lab3/test.sh
+++ l41-labs/lab3/test.sh
@@ -2,6 +2,6 @@ OUT_FILE=test.log
 
 sudo rm ${OUT_FILE}
 
-sudo dtrace -s $1 -o ${OUT_FILE} &
+sudo dtrace -s $1 -o ${OUT_FILE} -x switchrate=10hz -b 6m &
 ipc/ipc-static -i tcp -b 1048576 2thread && sudo kill -2 $!
 sudo cat ${OUT_FILE}
