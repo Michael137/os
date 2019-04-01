aac8d7dd3c115e21fc35b2eff1bca5061d3d5b70
Michael Buch <michaelbuch12@gmail.com>
2019-04-01 16:54:49 +0100

    measure congestion window and receive window

--- l41-labs/lab3/combined.d
+++ l41-labs/lab3/combined.d
@@ -4,23 +4,27 @@
 fbt::tcp_do_segment:entry
 /args[1]->th_sport == htons(10141)/
 {
-	printf("tx->rx ");
 	printf("source:%d ", htons(args[1]->th_sport));
 	printf("dest:%d ", htons(args[1]->th_dport));
 	printf("Seq:%d ", (unsigned int)args[1]->th_seq);
 	printf("Ack:%d ", (unsigned int)args[1]->th_ack);
 	printf("Time:%d ", walltimestamp);
+	printf("wnd:%d ", args[3]->snd_wnd);
+	printf("cwnd:%d ", args[3]->snd_cwnd);
+	printf("ssthresh:%d ", args[3]->snd_ssthresh);
 	trace(tcp_state_string[args[3]->t_state]);
 }
 
 fbt::tcp_do_segment:entry
 /args[1]->th_dport == htons(10141)/
 {
-	printf("rx->tx ");
 	printf("source:%d ", htons(args[1]->th_sport));
 	printf("dest:%d ", htons(args[1]->th_dport));
 	printf("Seq:%d ", (unsigned int)args[1]->th_seq);
 	printf("Ack:%d ", (unsigned int)args[1]->th_ack);
 	printf("Time:%d ", walltimestamp);
+	printf("wnd:%d ", args[3]->snd_wnd);
+	printf("cwnd:%d ", args[3]->snd_cwnd);
+	printf("ssthresh:%d ", args[3]->snd_ssthresh);
 	trace(tcp_state_string[args[3]->t_state]);
 }
---
+++ l41-labs/lab3/manual.sh
@@ -0,0 +1,6 @@
+OUT_FILE=$2
+
+rm ${OUT_FILE}
+
+dtrace -s $1 -o ${OUT_FILE} -x switchrate=5hz -b 50m &
+ipc/ipc-static -i tcp -b 1048576 -s 2thread && kill -2 $!
