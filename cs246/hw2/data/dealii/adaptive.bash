0a8afbca8b146ef43c1792d7250ff1444dbfd47f
Michael Buch <michaelbuch12@gmail.com>
2019-10-10 06:39:47 +0200

    plots

--- cs246/hw2/data/dealii/adaptive.bash
+++ cs246/hw2/data/dealii/adaptive.bash
@@ -2,12 +2,12 @@
 
 for hrt in `seq 4 16`
 do
-	OUT="adaptive_merged_${hrt}"
+	OUT="adaptive_merged_${hrt}.csv"
 	rm -f $OUT
 	touch $OUT
 
 	for pt in `seq 4 16`
 	do
-		grep "Percentage" "out/tool_adaptive_${pt}_${hrt}.out" | PT=$pt awk '{ print ENVIRON["PT"],$2 }' >> $OUT
+		grep "Percentage" "out/tool_adaptive_${pt}_${hrt}.out" | PT=$pt awk '{ print ENVIRON["PT"]","$2 }' >> $OUT
 	done
 done
