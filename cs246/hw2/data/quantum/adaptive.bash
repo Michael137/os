e434341b208e22c5a0b26c712608f91b173f5b78
Michael Buch <michaelbuch12@gmail.com>
2019-10-10 18:16:40 +0100

    graphs

--- cs246/hw2/data/quantum/adaptive.bash
+++ cs246/hw2/data/quantum/adaptive.bash
@@ -11,3 +11,15 @@ do
 		grep "Percentage" "out/tool_adaptive_${pt}_${hrt}.out" | PT=$pt awk '{ print ENVIRON["PT"]","$2 }' >> $OUT
 	done
 done
+
+for pt in `seq 4 16`
+do
+	OUT="adaptive_merged_pt_${pt}.csv"
+	rm -f $OUT
+	touch $OUT
+
+	for hrt in `seq 4 16`
+	do
+		grep "Percentage" "out/tool_adaptive_${pt}_${hrt}.out" | HHRT=$hrt awk '{ print ENVIRON["HHRT"]","$2 }' >> $OUT
+	done
+done
