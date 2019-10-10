dd25f425233de45c50bc3d27b3f5fdbcfb80e54b
Michael Buch <michaelbuch12@gmail.com>
2019-10-10 05:53:39 +0200

    data.collected.

---
+++ cs246/hw2/data/quantum/adaptive.bash
@@ -0,0 +1,13 @@
+#!/usr/local/bin/bash
+
+for hrt in `seq 4 16`
+do
+	OUT="adaptive_merged_${hrt}"
+	rm -f $OUT
+	touch $OUT
+
+	for pt in `seq 4 16`
+	do
+		grep "Percentage" "out/tool_adaptive_${pt}_${hrt}.out" | PT=$pt awk '{ print ENVIRON["PT"],$2 }' >> $OUT
+	done
+done
---
+++ cs246/hw2/data/quantum/collect.bash
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+
+PRED=$1
+OUT="${PRED}_merged"
+
+rm -f $OUT
+touch $OUT
+
+for i in `seq 4 16`
+do
+	grep "Percentage" "out/tool_${PRED}_${i}.out" | CTR=$i awk '{ print ENVIRON["CTR"], $2 }' >> $OUT
+done
