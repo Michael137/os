c4085a6bd0699adcda79481527001dfdba8d1df1
Michael Buch <michaelbuch12@gmail.com>
2019-10-21 21:37:56 +0200

    submission

---
+++ cs246/hw3/cppfiles/victim.h
@@ -0,0 +1,21 @@
+//here goes your implementation of the victim cache.
+
+#ifndef VICTIM_H_IN
+#define VICTIM_H_IN
+
+#include "cache.h"
+
+// Fully set-associative cache between
+// L1 and L2 DCache
+class VictimCache : public cache {
+
+public:
+	// Fully set-associative => associativity == number of cache lines
+	VictimCache( int blockSize, int totalCacheSize, int associativity, cache *nextLevel) :
+		cache( blockSize, totalCacheSize, associativity, nextLevel, true)
+	{}
+
+	bool isVictim() { return true; }
+};
+
+#endif
